7777e19f422f7fa1417ffcfa6e011c748cc097c5
Lord Vooja, the Guardian <60519144+VojtechVavra@users.noreply.github.com>
2022-05-07 13:01:01 +0200

    Update index.h

--- src/index.h
+++ src/index.h
@@ -1,18 +1,3 @@
-const char webpage2[] PROGMEM = R"=====(
-<!DOCTYPE html>
-<html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
-
-<h1>My First Heading</h1>
-<p>My first paragraph.</p>
-
-</body>
-</html>
-)=====";
-
 const char webpage[] PROGMEM = R"=====(
 <!DOCTYPE html>
 <html>
